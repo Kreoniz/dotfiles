909f5407c0cb04cc742dcc7d54af94d3cfd13886
Kreoniz <kreoniz@mail.ru>
2024-07-15 23:50:51 +0600

    feat(neovim): remove tailwind colorizer
    I didn't figure out how to properly set it up

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -916,13 +916,6 @@ require('lazy').setup({
       require('colorizer').setup()
     end,
   },
-  {
-    'themaxmarchuk/tailwindcss-colors.nvim',
-    event = 'BufEnter',
-    config = function()
-      require('tailwindcss-colors').setup()
-    end,
-  },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
