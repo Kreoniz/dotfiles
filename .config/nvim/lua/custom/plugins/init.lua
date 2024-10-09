a04143148310d1ba3da43117b3ff96614bf87e70
Kreoniz <kreoniz@mail.ru>
2024-10-09 19:02:04 +0600

    Update colorizer.nvim

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -4,8 +4,12 @@
 -- See the kickstart.nvim README for more information
 return {
   'norcalli/nvim-colorizer.lua',
-  event = 'BufEnter',
+  event = { 'BufReadPre', 'BufNewFile' }, -- Load on specific events
   config = function()
-    require('colorizer').setup()
+    require('colorizer').setup {
+      scss = { css = true, css_fn = true },
+      css = { css = true, css_fn = true, rgb_fn = true },
+      html = { rgb_fn = true },
+    }
   end,
 }
